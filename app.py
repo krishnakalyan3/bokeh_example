e56b8c809eee93ee0bb3f8bd7f0b8d1facfa8f57
krishnakalyan3 <krishnakalyan3@gmail.com>
2017-08-05 14:54:34 -0400

    code

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from perf_plot import plot
 from bokeh.embed import components
 from flask import Flask, flash, redirect, render_template, \
      request, url_for
+from conn_test import init_connection, get_backends, get_algos, get_release_time
 
 app = Flask(__name__)
 CORS(app)
@@ -13,15 +14,19 @@ CORS(app)
 
 @app.route("/")
 def chart():
-    warm_text = request.args.get('backend')
-    print(warm_text)
-    x = ['1.0', '2.7', '3.0']
-    y = [1.5, 2, 1.7]
+    backends_sel = request.args.get('backend', default='singlenode')
+    algos_sel = request.args.get('algo', default='total_time')
+
+    psql_conn = init_connection()
+    backends = get_backends(psql_conn)
+    algos = get_algos(psql_conn)
+    x, y = get_release_time(psql_conn, backends_sel, algos_sel)
+    print(x)
     plt = plot(x, y, title="")
     script1, div1 = components(plt)
     return render_template("child.html",  script1=script1, div1=div1,
-                           backend_type=[{'name':'spark_hybrid'}, {'name':'singlenode'}],
-                           algo_type= [{'name':'multinomial_data-gen_none_dense_10k_100'},
-                                       {'name':'multinomial_data-gen_none_sparse_10k_100'}])
+                           backend_type=backends,
+                           algo_type=algos)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug=True)
--- conn_test.py
+++ conn_test.py
@@ -4,7 +4,7 @@ import psycopg2
 import json
 from os.path import join
 import os
-
+import itertools
 
 def config_reader(read_path):
     with open(read_path, 'r') as input_file:
@@ -12,20 +12,47 @@ def config_reader(read_path):
 
     return conf_file
 
-if __name__ == '__main__':
-    FILE = join('creds', 'login.json')
+
+def get_backends(cursor):
+    cursor.execute("select distinct exec from perftest_2;")
+    backends = list(itertools.chain(*cursor.fetchall()))
+    return backends
+
+
+def get_algos(cursor):
+    cursor.execute("select distinct algo_comp from perftest_2;")
+    algos = list(itertools.chain(*cursor.fetchall()))
+    return algos
+
+
+def get_release_time(cursor, backend, algo):
+    print('######')
+    print(backend)
+    print(algo)
+    print('#####')
+    exec_string = "select time, tag from perftest_2 where algo_comp = '{}' and exec='{}';".format(algo, backend)
+    print(exec_string)
+    cursor.execute(exec_string)
+    rows = cursor.fetchall()
+    x = map(lambda x: x[1], rows)
+    y = map(lambda x: float(x[0]), rows)
+    return list(x), list(y)
+
+def init_connection():
     DIR = os.getcwd()
+    FILE = join('creds', 'login.json')
     PATH = join(DIR, FILE)
-
     login_info = config_reader(PATH)
-
-    conn_string = "host={} port={} dbname={} user={} password={}".format(login_info['host'],
-                                                                         login_info['port'],
-                                                                 login_info['dbname'],
-                                                                 login_info['user'],
-                                                                 login_info['password'])
+    conn_string = "host={} port={} dbname={} user={} password={}".format(
+    login_info['host'], login_info['port'], login_info['dbname'], login_info['user'], login_info['password'])
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
-    cursor.execute("select time, tag from perftest_2 where  algo_comp = 'multinomial_data-gen_none_dense_10k_100';")
-    rows = cursor.fetchall()
-    print(rows)
+    return cursor
+
+if __name__ == '__main__':
+    conn = init_connection()
+    #backend = get_backends(conn)
+    #algos = get_algos(conn)
+    x, y = get_release_time(conn, 'singlenode', 'multinomial_data-gen_none_dense_10k_100')
+    print(x)
+    print(y)
